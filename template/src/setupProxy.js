055ceeda3d2e4f037788037e300af21391676f3b
Ricky Roller <ricky.roller@domo.com>
2020-11-04 10:41:52 -0800

    Adding manifest management by default

--- template/src/setupProxy.js
+++ template/src/setupProxy.js
@@ -1,7 +1,21 @@
 const { Proxy } = require('@domoinc/ryuu-proxy');
-const manifest = require(`../public/${process.env.MANIFEST}`);
+const manifest = require(`../public/manifest.json`);
+const fs = require('fs');
 
-const config = { manifest };
+let tempManifest;
+const tempPath = `${process.cwd()}/src/manifest.tmp.json`;
+try {
+  tempManifest = fs.readFileSync(tempPath, 'utf8');
+  tempManifest = JSON.parse(tempManifest);
+  fs.unlinkSync(tempPath);
+} catch (e) {
+  // No overrides selected
+}
+const hasOverrides = tempManifest !== undefined;
+
+const config = {
+  manifest: hasOverrides ? tempManifest : manifest,
+};
 const proxy = new Proxy(config);
 
 module.exports = function(app) {
